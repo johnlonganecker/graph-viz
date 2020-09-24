40fb2d5dc9b01d452e5206642f8d4062c6089c70
john longanecker <johnlonganecker@gmail.com>
2020-09-23 22:41:36 -0700

    move out file watch to a sep function

--- server/server.js
+++ server/server.js
@@ -13,33 +13,41 @@ const wsServer = new WebSocketServer({
   httpServer: server
 });
 
+function watchFile(filename) {
+  return new Promise((success, fail) => {
+    let fsWait = false,
+      previousMD5 = null;
+
+    // https://thisdavej.com/how-to-watch-for-files-changes-in-node-js
+    fs.watch(file, (event, filename) => {
+      console.log('file modded');
+      if(filename && event === 'change') {
+        if(fsWait) return;
+
+        fsWait = setTimeout(() => {
+          fsWait = false;
+        }, 100);
+
+        let fileContents = fs.readFileSync(file),
+          currentMD5 = md5(fileContents);
+
+        if(currentMD5 !== previousMD5) {
+          console.log(`${filename} file changed`);
+          success(fileContents);
+          previousMD5 = currentMD5;
+        }
+      }
+    });
+  });
+}
+
 wsServer.on('request', request => {
   const conn = request.accept(null, request.origin),
     file = graphDataPath+'/graph-data/test.json';
 
-  let fsWait = false,
-    previousMD5 = null;
-
-  // https://thisdavej.com/how-to-watch-for-files-changes-in-node-js
-  fs.watch(file, (event, filename) => {
-    console.log('file modded');
-    if(filename && event === 'change') {
-      if(fsWait) return;
-
-      fsWait = setTimeout(() => {
-        fsWait = false;
-      }, 100);
-
-      let fileContents = fs.readFileSync(file),
-        currentMD5 = md5(fileContents);
-
-      if(currentMD5 !== previousMD5) {
-        console.log(`${filename} file changed`);
-        conn.sendUTF(fileContents);
-        previousMD5 = currentMD5;
-      }
-    }
-  });
+    watchFile(file).then(fileContents => {
+      conn.sendUTF(fileContents);
+    }).catch(() => {});
 
   conn.on('message', message => {
     let fileContents = fs.readFileSync(file);
